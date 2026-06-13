715795c0ef62c2f5a4deadcbeebace90ff5dd579
skyelighti <skyelighti.dev@gmail.com>
2026-06-13 15:54:08 -0700

    finished switching mat code
    removed uneessacry scripts as well.

--- CozyJam/Assets/Scripts/SwitchMat.cs
+++ CozyJam/Assets/Scripts/SwitchMat.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 public class SwitchMat : Interactable
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Material[] newmats;
+    Material[] ogmats;
+    Renderer matRenderer;
+
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        matRenderer = GetComponent<Renderer>();
     }
     public override void Interact()
     {
-        
+        ogmats = matRenderer.materials;
+        for (int i = 0; i < ogmats.Length; i++)
+        {
+            if (i < newmats.Length && newmats[i] != null)
+            {
+                ogmats[i] = newmats[i];
+            }
+        }
+
+        matRenderer.materials = newmats;
     }
 }
